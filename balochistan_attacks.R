d2ae3ec93cb668dc1c1edad6c83d0fe7cc947594
Somaiyah Hafeez <sh4625@columbia.edu>
2025-02-04 14:37:29 -0500

    Update balochistan_attacks.R

--- balochistan_attacks.R
+++ balochistan_attacks.R
@@ -1,5 +1,3 @@
-install.packages('tidyverse')
-install.packages("ggplot2")
 library(ggplot2)
 library(readr)
 library(stringr)
@@ -7,19 +5,18 @@ library(stringr)
 setwd('~/Desktop/in-one-chart')
 balochistan_df = read_csv('balochistan_cleaned.csv')
 
-filtered_df = balochistan_df[balochistan_df$month %in% c("2022-02","2022-05","2022-08","2022-12",
-                                                         "2023-02","2023-05","2023-08","2023-12", 
-                                                         "2024-02","2024-05","2024-08","2024-12"), ]
 
-filtered_df$month = as.Date(paste0(filtered_df$month, "-01"))
 
-ggplot(filtered_df, aes(x = month, y = event_count)) +  
-  geom_smooth(method = "loess", span = 0.2, color = "#D17A7A") +
+balochistan_df$month = as.Date(paste0(balochistan_df$month, "-01"))
+
+ggplot(balochistan_df, aes(x = month, y = event_count)) +  
+  geom_line(color = "#D17A7A") +
   scale_y_continuous(limits = c(0, 150),
                      breaks = seq(0, 150, 25)
                      ) +
-  scale_x_date(date_breaks = "6 months",
-               date_labels = "%b %Y") + 
+  scale_x_date(date_breaks = "1 year",
+               date_labels = "%b %Y",
+               limits = c(min(balochistan_df$month), max(balochistan_df$month))) + 
   labs(
     title = str_wrap("Violence in Balochistan surged during Pakistan's election month last year", width=95),
     subtitle = str_wrap("Separatist groups like Balochistan Liberation Army carried out 124 attacks in February alone. 
@@ -30,16 +27,16 @@ ggplot(filtered_df, aes(x = month, y = event_count)) +
   ) +
   
   
-  annotate("text", x = as.Date("2024-02-01"), y = 135, color="black",
+  annotate("text", x = as.Date("2024-02-01"), y = 135, color = "black",
            label = str_wrap("8 Feb. 2024: National and provincial assemblies elections were held", width = 30), 
-           size = 3, color = "black", family = "Arial")  +
+           size = 3, family = "Arial")  +
   geom_segment(aes(x = as.Date("2023-12-01"), xend = as.Date("2024-02-01"),
                    y = 124, yend = 124),  
                arrow = arrow(length = unit(0.2, "cm")), color = "black") +
   
-  annotate("text", x = as.Date("2024-08-01"), y = 100, color="black",
+  annotate("text", x = as.Date("2024-08-01"), y = 100, ccolor = "black",
            label = str_wrap("August 2024: Attacks intensified during Pakistan's independence month", width = 30), 
-           size = 3, color = "black",  family = "Arial") + 
+           size = 3, family = "Arial") + 
   geom_segment(aes(x = as.Date("2024-06-01"), xend = as.Date("2024-08-01"),
                    y = 87, yend = 87), 
                arrow = arrow(length = unit(0.2, "cm")), color = "black") +
@@ -48,4 +45,4 @@ ggplot(filtered_df, aes(x = month, y = event_count)) +
     text = element_text(family = "Arial", size = 12),
     plot.title = element_text(face = "bold"),
   )
-ggsave("~/Desktop/in-one-chart/balochistan_attacks.jpeg")
+ggsave("~/Desktop/in-one-chart/balochistan_attacks_1.0.jpeg")
